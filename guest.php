0d0ba1c08f132284d61e0365f8ac0cda09143e7c
Debdut Hira <55326254+debduthira@users.noreply.github.com>
2023-03-06 14:37:47 +0000

    fixed back button

--- guest.php
+++ guest.php
@@ -2,12 +2,17 @@
 	$name = $_POST['name'];
 	$address = $_POST['address'];
 	$phone = $_POST['phone'];
+// Set the database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hotelm";
 	
 require 'DB.php';
 
 	// Database connection
   
-	$conn = new mysqli(user='root' password='' host='localhost' port=3306 dbname='hotelm');
+	$conn = new mysqli($servername, $username, $password, $dbname);
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
